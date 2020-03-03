d2d76787661bc6ce215115f93ff604465d951e8b
AlyneAlvarenga <alynevs@gmail.com>
2020-03-03 15:46:34 -0500

    Updates

---
+++ precache-manifest.83f0f453dcedf95bece746bad1e006c3.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d756fa0f48fe2c78328dd2a8cff5c028",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d3396604db801e78d5d3",
+    "url": "/static/css/2.fa349ff9.chunk.css"
+  },
+  {
+    "revision": "0ed29285284a52769e77",
+    "url": "/static/css/main.631fd961.chunk.css"
+  },
+  {
+    "revision": "d3396604db801e78d5d3",
+    "url": "/static/js/2.8b35a7b2.chunk.js"
+  },
+  {
+    "revision": "4c86fd10a56eb60a384cd2f2c8426c35",
+    "url": "/static/js/2.8b35a7b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ed29285284a52769e77",
+    "url": "/static/js/main.d8e8d6df.chunk.js"
+  },
+  {
+    "revision": "6272beeee00e65b75576",
+    "url": "/static/js/runtime-main.6cfa7064.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
